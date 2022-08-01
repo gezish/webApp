26518d331193355290508271e4a20bda4fdf5b78
gezish <enggezahegn.w@gmail.com>
2022-08-01 22:42:37 +0300

    web deployed

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
 
-
+app = Flask(__name__)
 @app.route("/")
 def index():
-    pass
+    # Load current count
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+    # Increment the count
+    count += 1
+    
+    # Overwrite the count
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
 
-app = Flask(__name__)
+
+    # Render HTML with count variable
+    return render_template("index.html", count=count)
+
+
+if __name__ == "__main__":
+    app.run()
